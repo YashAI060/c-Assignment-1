34db09bc92f1fd4fe194da4a2bebedacdf502263
YashAI060 <yt888885@gmail.com>
2024-10-26 08:18:12 -0700

    Update Assignment#1.c

--- Assignment#1.c
+++ Assignment#1.c
@@ -8,17 +8,15 @@
 int main() {
     int a, b, sum;
     
-    // Reading two integers from the user
     printf("Enter the first integer: ");
     scanf("%d", &a);
     
     printf("Enter the second integer: ");
     scanf("%d", &b);
-    
-    // Calculating sum
+
     sum = a + b;
     
-    // Displaying the sum
+
     printf("The sum of %d and %d is: %d\n", a, b, sum);
     
     return 0;
@@ -32,14 +30,12 @@ int main() {
 int main() {
     int number, square;
     
-    // Reading an integer from the user
+
     printf("Enter an integer: ");
     scanf("%d", &number);
     
-    // Calculating the square
     square = number * number;
     
-    // Displaying the square
     printf("The square of %d is: %d\n", number, square);
     
     return 0;
@@ -53,14 +49,11 @@ int main() {
 int main() {
     float celsius, fahrenheit;
     
-    // Reading temperature in Celsius
     printf("Enter temperature in Celsius: ");
     scanf("%f", &celsius);
     
-    // Converting to Fahrenheit
     fahrenheit = (celsius * 9 / 5) + 32;
     
-    // Displaying the Fahrenheit temperature
     printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
     
     return 0;
@@ -74,14 +67,11 @@ int main() {
 int main() {
     float fahrenheit, celsius;
     
-    // Reading temperature in Fahrenheit
     printf("Enter temperature in Fahrenheit: ");
     scanf("%f", &fahrenheit);
-    
-    // Converting to Celsius
+
     celsius = (fahrenheit - 32) * 5 / 9;
-    
-    // Displaying the Celsius temperature
+
     printf("Temperature in Celsius: %.2f\n", celsius);
     
     return 0;
